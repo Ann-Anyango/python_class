a026a49db47430ce2ed238572d4fc31e65e56d09
Ann-Anyango <annmwangombe@gmail.com>
2023-07-29 21:04:05 +0300

    second changes

--- greenkiosk/settings.py
+++ greenkiosk/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -23,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-9j_xma8@lbe2x1d!e$m9p7*0r#+c78v5ey6-@8h=g)%+0moe17'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG =True
 
 ALLOWED_HOSTS = []
 
